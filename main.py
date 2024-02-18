84b02d54e82ab668255cb17f791b06a451202e4b
Erica Abrahms <erica.abrahms@instrument.com>
2024-02-18 14:20:52 -0800

    Add the RV circle

--- main.py
+++ main.py
@@ -6,19 +6,26 @@ from enum import Enum
 
 # CONSTANTS
 PIXELS_PER_FOOT = 2.4
+SMALL_FONT_SIZE = 6
+LARGE_FONT_SIZE = 20
 # System dependent. There is a default if we don't specify any font thing.
-SMALL_FONT = ImageFont.truetype(font='Roboto-Regular.ttf', size=6)
-LARGE_FONT = ImageFont.truetype(font='Roboto-Regular.ttf', size=20)
+SMALL_FONT = ImageFont.truetype(font='Roboto-Regular.ttf', size=SMALL_FONT_SIZE)
+LARGE_FONT = ImageFont.truetype(font='Roboto-Regular.ttf', size=LARGE_FONT_SIZE)
+
+CIRCLE_HEIGHT = 25
+BORDER_WIDTH = 2
+
+HEADER_HEIGHT = 25
+SIDE_HEIGHT = 15
 
 COLORS = {
     "SZ 1": "#6D9EEB",
     "SZ 2": "#C9DAF8",
     "SZ 3": "#00FFFF",
+    "BLACK": "#000000",
+    "WHITE": "#FFFFFF",
+    "PINK": "#FF00FF",
 }
-
-black = '#000000'
-white = '#FFFFFF'
-pink = '#FF00FF'
 
 # default vars for sample line
 frontage = 60
@@ -27,11 +34,12 @@ depth = 120
 def get_pixels_from_feet(distance_in_feet):
   return distance_in_feet * PIXELS_PER_FOOT
 
-def create_rectangle(drawer, text, width, height, color=black, bg="#FF00FF", gradient=None):
+def create_rectangle(drawer, text, width, height, color=COLORS["BLACK"], bg=COLORS["PINK"], gradient=None):
     i = Image.new("RGB", (width, height), bg)
     drawer = ImageDraw.Draw(i)
     # TODO: Font
     # TODO: add a border
+    # TODO: Wrap text
     drawer.text((5, 5), text, fill=color)
 
     return i
@@ -40,11 +48,25 @@ def rotate(thing_to_rotate, rotation = 0):
     # expand means "make the image bigger if necessary to accomodate"
     return thing_to_rotate.rotate(rotation, expand=1)
 
-def add_rectangle_to_image(image, rect, top_left):
+def add_obj_to_image(image, rect, top_left):
     image.paste(rect, box=top_left)
 
 def determine_rectangle_placement():
     return (50, 50);
+
+def create_circle_with_number(number, diam):
+    # TODO FIGURE OUT TRANSPARENT BG
+    i = Image.new("RGB", (diam + BORDER_WIDTH * 2, diam + BORDER_WIDTH * 2), COLORS["WHITE"])
+    drawer = ImageDraw.Draw(i)
+
+    tl = round((CIRCLE_HEIGHT - SMALL_FONT_SIZE) / 2)
+    # TODO: Font
+    # TODO: add a border
+    # TODO: Wrap text
+    drawer.arc((0,0, diam, diam), 0, 360, fill=COLORS["BLACK"], width=BORDER_WIDTH)
+    drawer.text((tl, tl), str(number), fill=COLORS["BLACK"])
+
+    return i
 
 # def add_gradient(drawer, rect):
 #     print (rect)
@@ -64,8 +86,8 @@ def determine_rectangle_placement():
 
 
 def create_horizontal_rectangle(drawer, text, fill, x=0, y=0, width=0, height=0):
-    drawer.rectangle((x, y, width, height), outline=black, width=1, fill=fill)
-    drawer.text((x+5, y+5), text=text, fill=black, font=SMALL_FONT)
+    drawer.rectangle((x, y, width, height), outline=COLORS["BLACK"], width=1, fill=fill)
+    drawer.text((x+5, y+5), text=text, fill=COLORS["BLACK"], font=SMALL_FONT)
 
 def create_sz_rectangle(drawer, sz):
     create_horizontal_rectangle(drawer, text=sz, width=100, fill=COLORS[sz], height=30)
@@ -83,64 +105,7 @@ def vertical_rectangle(text, width, height, color):
     return rotated
 
 
-def gen_image():
-    frontage_in_px = round(get_pixels_from_feet(frontage))
-    depth_in_px = round(get_pixels_from_feet(depth))
-
-    img = Image.new("RGB", (frontage_in_px, depth_in_px), white)
-
-    draw = ImageDraw.Draw(img)
-
-    # create_sz_rectangle(draw, "SZ 1")
-    HEADER_HEIGHT = 25
-    # Header
-    add_rectangle_to_image(
-        img,
-        create_rectangle(draw, "SZ 1", frontage_in_px, HEADER_HEIGHT),
-        (0,0) # start at top left.
-    )
-
-    # Left Bar
-    left = create_rectangle(draw, "LEFT BAR", depth_in_px - (HEADER_HEIGHT*2), HEADER_HEIGHT)
-    add_rectangle_to_image(
-        img,
-        left.rotate(-90, expand=1),
-        (0, HEADER_HEIGHT) # start at top left of left column
-    )
-
-    # Right Bar
-    right = create_rectangle(draw, "RIGHT BAR", depth_in_px - (HEADER_HEIGHT*2), HEADER_HEIGHT)
-    add_rectangle_to_image(
-        img,
-        right.rotate(-90, expand=1),
-        (frontage_in_px-HEADER_HEIGHT, HEADER_HEIGHT) # start at top left of right column
-    )
-
-    # Footer
-    add_rectangle_to_image(
-        img,
-        create_rectangle(draw, "FOOTER", frontage_in_px, HEADER_HEIGHT),
-        (0, img.height - HEADER_HEIGHT) # start at bottom left, offset by how tall the rectangle is.
-    )
-
-    # DIMS
-    # (FRONTAGE)
-    add_rectangle_to_image(
-        img,
-        create_rectangle(draw, str(frontage), HEADER_HEIGHT, HEADER_HEIGHT, bg=white, color=black),
-        (HEADER_HEIGHT, img.height - (2 * HEADER_HEIGHT)) # start at bottom left, offset by how tall the rectangle is.
-    )
-    # (DEPTH)
-    add_rectangle_to_image(
-        img,
-        create_rectangle(draw, str(depth), HEADER_HEIGHT, HEADER_HEIGHT, bg=white, color=black).rotate(-90, expand=1),
-        (HEADER_HEIGHT, img.height - (3 * HEADER_HEIGHT)) # start at bottom left, offset by how tall the rectangle is.
-    )
-
-
-    return img
-
-# Draw "works!" at 10,10  and fill it with black using our pre-defined font.
+# Draw "works!" at 10,10  and fill it with COLORS["BLACK"] using our pre-defined font.
 # draw.text((10, 10), text="Works!", fill=(0, 0, 0), font=FNT)
 
 # # Draw a 40x40 rectangle starting at 30,20
@@ -246,8 +211,8 @@ def read_csv():
             sound_zone = row['Sound Zone'] # e.g. "SZ 2"
 
             camps.append(CampInfo(
-                width=row['Frontage'],
-                height=row['Depth'],
+                width=int(row['Frontage']),
+                height=int(row['Depth']),
                 name=row[' '],
                 camp_type=camp_type, sound_zone=sound_zone, interactivity_time=interactivity_time, sound_size=sound_size,
                 neighborhood_preference=row['neighborhood'].split(' '),
@@ -259,15 +224,81 @@ def read_csv():
     return camps
 
 
+def gen_image_for_camp(camp: CampInfo):
+    frontage_in_px = round(get_pixels_from_feet(camp.width))
+    depth_in_px = round(get_pixels_from_feet(camp.height))
+
+    img = Image.new("RGB", (frontage_in_px, depth_in_px), COLORS["WHITE"])
+
+    draw = ImageDraw.Draw(img)
+
+
+    # Header
+    add_obj_to_image(
+        img,
+        create_rectangle(draw, camp.sound_zone, frontage_in_px, HEADER_HEIGHT),
+        (0,0) # start at top left.
+    )
+
+    # Camp Name
+    add_obj_to_image(
+        img,
+        create_rectangle(draw, camp.name, frontage_in_px - HEADER_HEIGHT*2, HEADER_HEIGHT, bg=COLORS["WHITE"]),
+        (HEADER_HEIGHT, HEADER_HEIGHT),
+    )
+
+    # Left Bar
+    left = create_rectangle(draw, "LEFT BAR", depth_in_px - (HEADER_HEIGHT*2), HEADER_HEIGHT)
+    add_obj_to_image(
+        img,
+        left.rotate(-90, expand=1),
+        (0, HEADER_HEIGHT) # start at top left of left column
+    )
+
+    # Right Bar
+    right = create_rectangle(draw, "RIGHT BAR", depth_in_px - (HEADER_HEIGHT*2), HEADER_HEIGHT)
+    add_obj_to_image(
+        img,
+        right.rotate(-90, expand=1),
+        (frontage_in_px-HEADER_HEIGHT, HEADER_HEIGHT) # start at top left of right column
+    )
+
+    # Footer
+    add_obj_to_image(
+        img,
+        create_rectangle(draw, "FOOTER", frontage_in_px, HEADER_HEIGHT),
+        (0, img.height - HEADER_HEIGHT) # start at bottom left, offset by how tall the rectangle is.
+    )
+
+    # DIMS
+    # (FRONTAGE)
+    add_obj_to_image(
+        img,
+        create_rectangle(draw, str(camp.width), HEADER_HEIGHT, HEADER_HEIGHT, bg=COLORS["WHITE"], color=COLORS["BLACK"]),
+        (HEADER_HEIGHT, img.height - (2 * HEADER_HEIGHT)) # start at bottom left, offset by how tall the rectangle is.
+    )
+    # (DEPTH)
+    add_obj_to_image(
+        img,
+        create_rectangle(draw, str(camp.height), HEADER_HEIGHT, HEADER_HEIGHT, bg=COLORS["WHITE"], color=COLORS["BLACK"]).rotate(-90, expand=1),
+        (HEADER_HEIGHT, img.height - (3 * HEADER_HEIGHT)) # start at bottom left, offset by how tall the rectangle is.
+    )
+
+    # RV Circle
+    add_obj_to_image(
+        img,
+        create_circle_with_number(3, CIRCLE_HEIGHT),
+        (frontage_in_px - (HEADER_HEIGHT + CIRCLE_HEIGHT), img.height - (HEADER_HEIGHT + CIRCLE_HEIGHT)) # start at bottom left, offset by how tall the rectangle is.
+    )
+
+    return img
+
 
 
 # with open('img.jpg', 'w') as f:
 #     img.save(f)
 if __name__ == '__main__':
-    import sys
-    if len(sys.argv) > 1 and sys.argv[1] == 'csv':
-        camps = read_csv()
-        import pdb; pdb.set_trace()
-    else:
-        img = gen_image()
-        img.show()
+    camps = read_csv()
+    camp = camps[0]
+    img = gen_image_for_camp(camp)
+    img.show()
